0741aafa422a8b513c7e0d7771c503803530dc9a
Vladimir__Lipodat <sibi_7@mail.ru>
2017-07-13 00:22:46 +0300

    add section real work

--- app/js/script.js
+++ app/js/script.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     $(".our-results__body .left").animated("slideInUp");
     $(".our-results__body .center").animated("slideInUp");
     $(".our-results__body .right").animated("slideInUp");
+    $(".real-work__company__item").animated("zoomIn");
     /*clouds animation*/
     var x = 0;
     setInterval(function () {
@@ -20,4 +21,48 @@ $(document).ready(function () {
         var pos = (y += 1) + 'px 0';
         $('.header__absolute2').css('background-position', pos);
     }, 60);
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: true,
+        navigation: true,
+        navigationText: true,
+        pagination: true,
+        // autoplay: true,
+        // autoplayTimeout: 5000,
+        items: 3,
+        dots: true,
+        // singleItem: false,
+        // mouseDrag: false,
+        // touchDrag: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: true
+            },
+            1000: {
+                items: 1,
+                nav: true,
+                loop: true
+            },
+            1200: {
+                items: 3,
+                nav: true
+            }
+        }
+    });
+    $('.test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+        // other options
+    });
 });
