707ae0aaeb9461d70bfd672b4245dbeaff2eee9d
Vladimir__Lipodat <sibi_7@mail.ru>
2017-07-13 23:47:23 +0300

    add animation footer for balloon

--- app/js/script.js
+++ app/js/script.js
@@ -90,4 +90,20 @@ $(document).ready(function () {
             $('body,html').animate({scrollTop: top}, 1500);
         });
     });
+    var b = document.getElementsByClassName("footer__absolute-top")[0];
+
+    b.addEventListener("mousemove", function(event) {
+        parallaxed(event);
+
+    });
+
+    function parallaxed(e) {
+        var amountMovedX = (e.clientX * -0.4 / 8);
+        var amountMovedY = (e.clientY * -0.4 / 8);
+        var x = document.getElementsByClassName("footer__absolute-top");
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].style.transform='translate(' + amountMovedX + 'px,' + amountMovedY + 'px)'
+        }
+    }
 });
